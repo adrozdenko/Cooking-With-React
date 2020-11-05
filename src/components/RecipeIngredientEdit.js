cea4d75e720ff284006328d12fd01fc001a3820e
andrii.lito <andrii.lito@gmail.com>
2020-11-05 02:07:16 +0200

    onChange instead of onInput

--- src/components/RecipeIngredientEdit.js
+++ src/components/RecipeIngredientEdit.js
@@ -11,12 +11,12 @@ export default function RecipeIngredientEdit(props) {
       <input
         className='recipe-edit__input'
         defaultValue={ingredient.name}
-        onInput={(e) => handleChange({ name: e.target.value })}
+        onChange={(e) => handleChange({ name: e.target.value })}
       />
       <input
         className='recipe-edit__input'
         defaultValue={ingredient.amount}
-        onInput={(e) => handleChange({ amount: e.target.value })}
+        onChange={(e) => handleChange({ amount: e.target.value })}
       />
       <button
         className='btn btn--danger'
